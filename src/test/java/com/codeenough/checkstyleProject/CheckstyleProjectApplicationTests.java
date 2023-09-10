43a75d1786c1f540bd02f3b47e5762a87c21a786
gusgraujo <gusg.raujo@gmail.com>
2023-09-10 22:17:16 +0100

    Init project

---
+++ src/test/java/com/codeenough/checkstyleProject/CheckstyleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeenough.checkstyleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
